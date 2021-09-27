70e908d53efa61f57dbe97c44f0ee352e64355cf
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-27 20:28:54 -0300

    Initial commit

---
+++ Models/masterContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace cadastro_hospital.Models
+{
+    public partial class masterContext : DbContext
+    {
+        public masterContext()
+        {
+        }
+
+        public masterContext(DbContextOptions<masterContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost;Database=master;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
