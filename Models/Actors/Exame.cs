7fd708529f9ccdf24b82eda35a226dd67557f9c8
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-27 20:33:00 -0300

    Modelos diversos

--- Models/Actors/Exame.cs
+++ Models/Actors/Exame.cs
@@ -3,6 +3,13 @@ using System;
 
 namespace cadastro_hospital.Models {
     public class Exame {
+        [Key]
+        public int id {get; set;}
+        public int tipoexame_id {get; set;}
+        [MaxLength(100)]
+        public string nome {get; set;}
+        [MaxLength(256)]
+        public string descricao {get; set;}
 
     }
 }
