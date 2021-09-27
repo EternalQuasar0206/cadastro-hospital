9d1304e966b9a324a637d32be40ea3682182251f
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-27 20:30:50 -0300

    Modelo - Paciente

--- Models/Actors/Paciente.cs
+++ Models/Actors/Paciente.cs
@@ -1,5 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
 namespace cadastro_hospital.Models {
     public class Paciente {
-        
+        [Key]
+        public int id {get; set;}
+        [MaxLength(100)]
+        public string nome {get; set;}
+        [MaxLength(11)]
+        public string cpf {get; set;}
+        public DateTime nascimento {get; set;}
+        public char sexo {get; set;}
+        public string telefone {get; set;}
+        public string email {get; set;}
     }
 }
