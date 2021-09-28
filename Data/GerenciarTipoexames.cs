aaf167955235879ec8cd30931386612d26b427f1
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-27 21:45:48 -0300

    Classe "Gerenciar Tipoexames"

---
+++ Data/GerenciarTipoexames.cs
@@ -0,0 +1,55 @@
+using cadastro_hospital.Models;
+using System.Linq;
+using System.Text.Json;
+using System;
+
+namespace cadastro_hospital.Data {
+    //TODO: Adicionar validadores de informação
+    public static class GerenciarTipoexames {
+        public static string Novo(TipoExame tipoexame) {
+            cadastro_hospitalContext ctx = new cadastro_hospitalContext();
+
+            try {
+                ctx.TipoExames.Add(tipoexame);
+                return JsonSerializer.Serialize(tipoexame);
+            }
+            catch(Exception e) {
+                return JsonSerializer.Serialize(new Erro() {
+                    mensagem = "Ocorreu um erro ao realizar a ação: " + e
+                });
+            }
+        }
+
+        public static string Alterar(int id, TipoExame tipoexame) {
+            cadastro_hospitalContext ctx = new cadastro_hospitalContext();
+            TipoExame entity = ctx.TipoExames.Where(x => x.Id == id).FirstOrDefault();
+            if (entity == null)
+            {
+                return JsonSerializer.Serialize(new Erro() {
+                    mensagem = "O id do tipo de exame informado não existe"
+                });
+            }
+
+            ctx.Entry(entity).CurrentValues.SetValues(tipoexame);
+            ctx.SaveChanges();
+            return JsonSerializer.Serialize(entity);
+        }
+
+        public static string Excluir(int id) {
+            cadastro_hospitalContext ctx = new cadastro_hospitalContext();
+
+            try {
+                TipoExame entity = ctx.TipoExames.Where(x => x.Id == id).FirstOrDefault();
+                ctx.TipoExames.Remove(entity);
+                ctx.SaveChanges();
+                return JsonSerializer.Serialize(entity);
+            }
+            catch(Exception e) {
+                return JsonSerializer.Serialize(new Erro() {
+                    mensagem = "Ocorreu um erro ao realizar a ação: " + e
+                });
+            }
+
+        }
+    }
+}
