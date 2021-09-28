a4499a602c4976d9ee859e193cdc864f447fe863
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-28 09:24:12 -0300

    Atualizações - View

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var dq = function(x) {
+    return document.querySelector(x);
+}
 
-// Write your JavaScript code.
+function CadastrarPaciente() {
+    let data = {
+        Nome: dq("#nome_paciente").value,
+        Cpf: dq("#cpf_paciente").value,
+        Nascimento: dq("#nascimento_paciente").value,
+        Sexo: dq("#sexo_paciente").options[dq("#sexo_paciente").selectedIndex].value,
+        Email: dq("#email_paciente").value,
+        Telefone: dq("#telefone_paciente").value
+    }
+
+    fetch("../Api/Paciente", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.mensagem) {
+            alert(data.mensagem);
+        }
+        else {
+            alert("Paciente criado com sucesso")
+        }
+    })
+    .catch((error) => {
+        alert("Ocorreu um erro: " + error)
+    })
+}
