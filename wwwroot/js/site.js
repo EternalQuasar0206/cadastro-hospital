5946548580fef9b3edc6bc988cc0cb2c7f66c752
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-28 09:42:28 -0300

    Atualizações - View/Script

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -32,3 +32,35 @@ function CadastrarPaciente() {
         alert("Ocorreu um erro: " + error)
     })
 }
+
+function EditarPaciente() {
+    let data = {
+        Id: dq("#id_paciente").value,
+        Nome: dq("#enome_paciente").value,
+        Cpf: dq("#ecpf_paciente").value,
+        Nascimento: dq("#enascimento_paciente").value,
+        Sexo: dq("#esexo_paciente").options[dq("#esexo_paciente").selectedIndex].value,
+        Email: dq("#eemail_paciente").value,
+        Telefone: dq("#etelefone_paciente").value
+    }
+
+    fetch("../Api/Paciente", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.mensagem) {
+            alert(data.mensagem);
+        }
+        else {
+            alert("Paciente editado com sucesso")
+        }
+    })
+    .catch((error) => {
+        alert("Ocorreu um erro: " + error)
+    })
+}
