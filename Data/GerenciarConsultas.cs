b8c62d1b1323a929bf3af93cd4ad0b88a45d06c7
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-28 17:30:05 -0300

    Correção - Gerenciador de consultas

--- Data/GerenciarConsultas.cs
+++ Data/GerenciarConsultas.cs
@@ -11,22 +11,20 @@ namespace cadastro_hospital.Data {
             try {
                 var paciente = ctx.Pacientes.Where(x => x.Id == consultaRequest.paciente).FirstOrDefault();
                 var exame = ctx.Exames.Where(x => x.Id == consultaRequest.exame).FirstOrDefault();
-                var concorrente_data = ctx.Consultas.Where(x => x.Data == consultaRequest.data).FirstOrDefault();
+                var concorrente = ctx.Consultas.Where(x => x.Data == consultaRequest.data).SingleOrDefault();
 
-                if(concorrente_data.Data != null) {
-                    var consulta = new Consulta() {
-                        Paciente = consultaRequest.paciente,
-                        Exame = consultaRequest.exame,
-                        Data = consultaRequest.data,
-                        Protocolo = (int)new Random().Next(1, 999999) + DateTime.Now.ToString("yyyyMMddHHmmss")
-                    };
-                    ctx.Consultas.Add(consulta);
-                    ctx.SaveChanges();
-                    return JsonSerializer.Serialize(consulta);
+                if(!IsDate(consultaRequest.data)) {
+                    throw new Exception("Data inválida");
                 }
-                else {
-                    throw new Exception("Já existe uma consulta marcada nesta data.");
-                }
+                var consulta = new Consulta() {
+                    Paciente = paciente.Id,
+                    Exame = exame.Id,
+                    Data = consultaRequest.data,
+                    Protocolo = (int)new Random().Next(1, 999999) + DateTime.Now.ToString("yyyyMMddHHmmss")
+                };
+                ctx.Consultas.Add(consulta);
+                ctx.SaveChanges();
+                return JsonSerializer.Serialize(consulta);
             }
             catch(Exception e) {
                 return JsonSerializer.Serialize(new Erro() {
@@ -55,6 +53,18 @@ namespace cadastro_hospital.Data {
             cadastro_hospitalContext ctx = new cadastro_hospitalContext();
             var consultas = ctx.Consultas.ToList();
             return JsonSerializer.Serialize(consultas);
+        }
+
+        public static bool IsDate(string tempDate) { 
+            DateTime fromDateValue; 
+            var formats = new[] { "dd/MM/yyyy", "yyyy-MM-dd" }; 
+            if (DateTime.TryParseExact(tempDate, formats, 
+            System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out fromDateValue)) { 
+                return true;
+            }
+            else { 
+                return false;
+            }
         }
     }
 }
